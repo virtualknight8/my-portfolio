abd4a37a319409b6095b1c6bddc938882d6ab46f
virtualknight8 <ankitkhola2@gmail.com>
2023-07-19 22:20:33 +0530

    add new project and resume

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const portfolioViewSource = document.querySelector('#portview');
 const sudokuViewSource = document.querySelector('#sudoview');
 const resumebtn = document.getElementById('resumebutton');
 const sourceButton = document.getElementById('sourcebutton');
+const landingPageTryButton = document.getElementById('landingpagetry');
+const landingPageSourceButton = document.getElementById('landingpagesource');
 
 sudokuTryButton.addEventListener('click', ()=>{
     window.location.href='https://virtualknight8.github.io/sudoku-helper/';
@@ -24,9 +26,17 @@ sudokuViewSource.addEventListener('click', ()=>{
 });
 
 resumebtn.addEventListener('click', ()=>{
-    window.location.href='src/final resume.pdf';
+    window.location.href='src/Updated Resume.pdf';
 }
 );
+
+landingPageTryButton.addEventListener('click', ()=>{
+    window.location.href='https://new-landing-page-flax.vercel.app/';
+});
+
+landingPageSourceButton.addEventListener('click', ()=>{
+    window.location.href='https://github.com/virtualknight8/new-landing-page.git';
+});
 
 sourceButton.addEventListener('click', ()=>{
     window.location.href='https://github.com/virtualknight8/inventory-management';
